0264b71981deb2885602ea2fefc898ed3f7eb27d
Tim Kemmis <kemmojr@gmail.com>
2024-11-02 11:56:15 +1100

    Got redrawing at the other side of the screen working

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+
 func _physics_process(_delta: float) -> void:
 	
 	# Handle jump.
@@ -26,11 +27,14 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	var screenSize = get_viewport().get_visible_rect().size
+
 	if (velocity.x > 0):
-		print('x',velocity.x)
-		print('y:',velocity.y)
 		global_position = Vector2(0, global_position.y)
-	else:
-		print('x',velocity.x)
-		print('y:',velocity.y)
+	elif velocity.x < 0:
+		global_position = Vector2(screenSize.x, global_position.y)
+		print(global_position)
+	elif velocity.y > 0:
 		global_position = Vector2(global_position.x, 0)
+	elif velocity.y < 0:
+		global_position = Vector2(global_position.x, screenSize.y)
