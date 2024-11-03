694889e5c30218c74503104b125635794ba3f1bb
Tim Kemmis <kemmojr@gmail.com>
2024-11-03 21:14:58 +1100

    tried to make the sprite rotate for the snake

--- Scenes/food.gd
+++ Scenes/food.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 @onready var player = get_node("/root/main/Player")
 func _on_body_entered(body: Node2D) -> void:
-    if body == player:
+    if body == player and player.has_method("eat"):
+        var direct_to_player = global_position.direction_to(player.global_position)
+        player.eat(direct_to_player)
         queue_free()
 
--- Scenes/player.gd
+++ Scenes/player.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const segmentX = 15
+const segmentY = 14
+@onready var current = $"."
 
 
 func _physics_process(_delta: float) -> void:
@@ -11,30 +14,57 @@ func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("jump"):
 		velocity.x = 0
 		velocity.y = 0 - SPEED
+		if rotation - Vector2.UP.angle() != 0:
+			rotate(Vector2.UP.angle())
+
 		
 	if Input.is_action_just_pressed("down"):
 		velocity.x = 0
 		velocity.y = SPEED
+		if rotation - Vector2.DOWN.angle() != 0:
+			rotate(Vector2.DOWN.angle())
 
 	if Input.is_action_just_pressed("left"):
 		velocity.y = 0
 		velocity.x = 0 - SPEED
+		if rotation - Vector2.LEFT.angle() != 0:
+			rotate(Vector2.LEFT.angle())
 		
 	if Input.is_action_just_pressed('right') or (velocity.x == 0 and velocity.y == 0):
 		velocity.y = 0
 		velocity.x = SPEED
+		if rotation - Vector2.RIGHT.angle() != 0:
+			rotate(Vector2.RIGHT.angle())
+
 	
 	move_and_collide(velocity * _delta)
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	var screenSize = get_viewport().get_visible_rect().size
 
+	# right
 	if (velocity.x > 0):
 		global_position = Vector2(0, global_position.y)
+	# left
 	elif velocity.x < 0:
 		global_position = Vector2(screenSize.x, global_position.y)
-		print(global_position)
+	# down
 	elif velocity.y > 0:
 		global_position = Vector2(global_position.x, 0)
+	# up
 	elif velocity.y < 0:
 		global_position = Vector2(global_position.x, screenSize.y)
+
+func eat(direction: Vector2):
+	return
+	var collider: CollisionShape2D = current.get_node("Collider").duplicate()
+	var sprite: AnimatedSprite2D = current.get_node("Sprite").duplicate()
+	collider.name += "_segment"
+	sprite.position = global_position + direction
+	collider.position = global_position + direction
+
+
+	current.add_child(sprite)
+	current.add_child(collider)	
+
+
