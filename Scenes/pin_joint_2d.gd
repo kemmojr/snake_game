a388e8459bb7853729a92ab71ae4074a7ab79191
Tim Kemmis <kemmojr@gmail.com>
2024-11-12 22:04:21 +1100

    added tweening

--- Scenes/pin_joint_2d.gd
+++ Scenes/pin_joint_2d.gd
@@ -3,14 +3,6 @@ extends PinJoint2D
 var rotation_delay = 0.0
 
 func _on_player_rotated_to(new_rotation_val:float) -> void:
-    print("rotation", rotation, "new_rotation", new_rotation_val)
+    var tween = get_tree().create_tween()
     rotation = new_rotation_val
-    rotation_delay = 1.0
-
-func _physics_process(delta: float) -> void:
-    if rotation_delay <= 0:
-        rotation = 0
-        return 
-    
-    rotation_delay -= delta
-    print("rotation_delay", rotation_delay)
+    tween.tween_property($".", "rotation", 0, 0.3)
--- Scenes/player.gd
+++ Scenes/player.gd
@@ -5,73 +5,85 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const segmentX = 15
 const segmentY = 14
+
 signal rotatedTo(new_rotation: float)
+
 @onready var current = $"."
 
 
 func _physics_process(delta: float) -> void:
 
-	var old_rotation = rotation
-	# Handle jump.
-	if Input.is_action_just_pressed("jump"):
-		velocity.x = 0
-		velocity.y = 0 - SPEED
-		if (rotation - Vector2.UP.angle()) != 0:
-			rotation = Vector2.UP.angle()
+    var old_rotation = rotation
+    # Handle jump.
+    if Input.is_action_just_pressed("jump"):
+        velocity.x = 0
+        velocity.y = 0 - SPEED
+        if (rotation - Vector2.UP.angle()) != 0:
+            rotation = Vector2.UP.angle()
 
-		
-	if Input.is_action_just_pressed("down"):
-		velocity.x = 0
-		velocity.y = SPEED
-		if rotation - Vector2.DOWN.angle() != 0:
-			rotation = Vector2.DOWN.angle()
+        
+    if Input.is_action_just_pressed("down"):
+        velocity.x = 0
+        velocity.y = SPEED
+        if rotation - Vector2.DOWN.angle() != 0:
+            rotation = Vector2.DOWN.angle()
 
-	if Input.is_action_just_pressed("left"):
-		velocity.y = 0
-		velocity.x = 0 - SPEED
-		if rotation - Vector2.LEFT.angle() != 0:
-			rotation = Vector2.LEFT.angle()
-		
-	if Input.is_action_just_pressed('right') or (velocity.x == 0 and velocity.y == 0):
-		velocity.y = 0
-		velocity.x = SPEED
-		if rotation - Vector2.RIGHT.angle() != 0:
-			rotation = Vector2.RIGHT.angle()
+    if Input.is_action_just_pressed("left"):
+        velocity.y = 0
+        velocity.x = 0 - SPEED
+        if rotation - Vector2.LEFT.angle() != 0:
+            rotation = Vector2.LEFT.angle()
+        
+    if Input.is_action_just_pressed('right') or (velocity.x == 0 and velocity.y == 0):
+        velocity.y = 0
+        velocity.x = SPEED
+        if rotation - Vector2.RIGHT.angle() != 0:
+            rotation = Vector2.RIGHT.angle()
 
-	if (old_rotation != rotation):
-		rotatedTo.emit(rotation)
-	move_and_collide(velocity * delta)
+    if (old_rotation != rotation):
+        var current = rotation
+        print("v",velocity)
+        print("old",old_rotation)
+        print("current", current)
+        print("calc",(abs(old_rotation) - abs(rotation)))
+        var target_rotation = atan2(velocity.y, velocity.x)
+        var angle_difference = target_rotation - current
+        
+        # Normalize the angle difference to [-π, π]
+        angle_difference = fmod(angle_difference + PI, TAU) - PI
+        rotatedTo.emit(abs(old_rotation) - abs(rotation))
+    move_and_collide(velocity * delta)
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-	var screenSize = get_viewport().get_visible_rect().size
+    var screenSize = get_viewport().get_visible_rect().size
 
-	# right
-	if (velocity.x > 0):
-		global_position = Vector2(0, global_position.y)
-	# left
-	elif velocity.x < 0:
-		global_position = Vector2(screenSize.x, global_position.y)
-	# down
-	elif velocity.y > 0:
-		global_position = Vector2(global_position.x, 0)
-	# up
-	elif velocity.y < 0:
-		global_position = Vector2(global_position.x, screenSize.y)
+    # right
+    if (velocity.x > 0):
+        global_position = Vector2(0, global_position.y)
+    # left
+    elif velocity.x < 0:
+        global_position = Vector2(screenSize.x, global_position.y)
+    # down
+    elif velocity.y > 0:
+        global_position = Vector2(global_position.x, 0)
+    # up
+    elif velocity.y < 0:
+        global_position = Vector2(global_position.x, screenSize.y)
 
 func eat(direction: Vector2):
-	return
-	var collider: CollisionShape2D = current.get_node("Collider").duplicate()
-	var sprite: AnimatedSprite2D = current.get_node("Sprite").duplicate()
-	collider.name += "_segment"
-	sprite.position = global_position + direction
-	collider.position = global_position + direction
+    return
+    var collider: CollisionShape2D = current.get_node("Collider").duplicate()
+    var sprite: AnimatedSprite2D = current.get_node("Sprite").duplicate()
+    collider.name += "_segment"
+    sprite.position = global_position + direction
+    collider.position = global_position + direction
 
 
-	current.add_child(sprite)
-	current.add_child(collider)	
+    current.add_child(sprite)
+    current.add_child(collider)	
 
 
 
 
 func _on_rotated_to(new_rotation:float) -> void:
-	pass # Replace with function body.
+    pass # Replace with function body.
