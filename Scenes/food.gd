5e473d3a4dfdccb9afc4ae82e9a5bc1bd37eafa6
Tim Kemmis <kemmojr@gmail.com>
2024-11-14 21:47:00 +1100

    Got the one food being eaten and respawning elswhere

--- Scenes/food.gd
+++ Scenes/food.gd
@@ -1,8 +1,14 @@
 extends Area2D
 
 @onready var player = get_node("/root/main/Player")
+@onready var food_collider = $CollisionShape2D
 func _on_body_entered(body: Node2D) -> void:
+    var screenSize = get_viewport().get_visible_rect().size
+    var food_radius = food_collider.shape.radius
     if body == player and player.has_method("eat"):
         var direct_to_player = global_position.direction_to(player.global_position)
         player.eat(direct_to_player)
-        queue_free()
+        var x = randi_range(0, screenSize.x - food_radius)
+        var y = randi_range(0, screenSize.y - food_radius)
+        
+        global_position = Vector2(x, y)
