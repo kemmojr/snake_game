5ac18622b4313d787a691d356b8e02f23427aa02
Tim Kemmis <kemmojr@gmail.com>
2024-11-14 21:18:52 +1100

    Got movement rotation looking more fluid

--- Scenes/pin_joint_2d.gd
+++ Scenes/pin_joint_2d.gd
@@ -2,12 +2,14 @@ extends PinJoint2D
 
 var tween_len = 0.3
 signal rotatedTo(new_rotation: float)
+signal finished()
 
 func _on_player_rotated_to(new_rotation_val:float) -> void:
     var tween = get_tree().create_tween()
     rotation = new_rotation_val
-    tween.tween_property($".", "rotation", 0, tween_len)
-    tween.tween_callback(emit.bind(new_rotation_val))
-    
-func emit(new_rotation_val: float):
     rotatedTo.emit(new_rotation_val)
+    tween.tween_property($".", "rotation", 0, tween_len)
+    tween.tween_callback(emit_finished)
+    
+func emit_finished():
+    finished.emit()
--- Scenes/second_joint.gd
+++ Scenes/second_joint.gd
@@ -3,12 +3,18 @@ extends PinJoint2D
 
 var tween_len = 0.3
 signal rotatedTo(new_rotation: float)
+signal finished()
 
 func _on_pin_joint_2d_rotated_to(new_rotation_val: float) -> void:
-    var tween = get_tree().create_tween()
     rotation = new_rotation_val
-    tween.tween_property($".", "rotation", 0, tween_len)
-    tween.tween_callback(emit.bind(new_rotation_val))
-    
-func emit(new_rotation_val: float):
     rotatedTo.emit(new_rotation_val)
+    
+
+
+func _on_pin_joint_2d_finished() -> void:
+    var tween = get_tree().create_tween()
+    tween.tween_property($".", "rotation", 0, tween_len)
+    tween.tween_callback(emit_finished)
+    
+func emit_finished():
+    finished.emit()
